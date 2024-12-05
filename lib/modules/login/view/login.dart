9c37deff664df0ff45df07df11865beb7f5a4d40
Cpaluszek <paluszek.clement@proton.me>
2024-12-05 10:27:14 +0100

    Improve login page

--- lib/modules/login/view/login.dart
+++ lib/modules/login/view/login.dart
@@ -1,5 +1,7 @@
+import 'package:catppuccin_flutter/catppuccin_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:provider/provider.dart';
 import 'package:swifty_companion/modules/login/bloc/auth_bloc.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -11,14 +13,47 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final flavor = Provider.of<Flavor>(context);
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            context.read<AuthBloc>().add(LoginRequested());
-          },
-          child: const Text('Login'),
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              flavor.base,
+              flavor.crust,
+            ],
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome to Swifty Companion!',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 40),
+            Center(
+              child: ElevatedButton.icon(
+                onPressed: () {
+                  context.read<AuthBloc>().add(LoginRequested());
+                },
+                label: const Text('Sign in with'),
+                icon: SvgPicture.asset(
+                  'assets/42_Logo.svg',
+                  height: 22,
+                ),
+                iconAlignment: IconAlignment.end,
+              ),
+            ),
+          ],
         ),
       ),
     );
